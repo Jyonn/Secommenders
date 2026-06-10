aa436eb6a7b319634b3ed79e2d1c29efd39f2bef
Jyonn <liu@qijiong.work>
2026-06-10 23:17:01 +0800

    Remove backend auth from local scheduler

--- local_scheduler.py
+++ local_scheduler.py
@@ -3,45 +3,6 @@ from pathlib import Path
 from utils import Schedule
 
 
-DATASETS = ['mind', 'recifvideo', 'recifvideolarge', 'recifvideoxlarge']
-LLM_MODELS = ['llama3', 'qwen35th9b', 'qwen35th4b', 'qwen35th08b']
-SCRATCH_MODELS = ['scratch']
-SOURCE_MODELS = ['llama3', 'qwen3embedding06b']
-LLM_HISTORIES = ['uid', 'text', 'sid', 'embedding']
-SCRATCH_HISTORIES = ['uid', 'sid', 'embedding']
-SID_VARIANTS = [('rqvae', 'recon')]
-UID_VARIANTS = ['flat', ('hierarchical', '20', '3,20')]
-
-
-def build_basic_schedule():
-    return (
-        Schedule(
-            name='basic',
-            backend_auth='5ec0mmender5',
-            backend_host='https://api.seco.qijiong.work',
-            effective_batch_size=64,
-        )
-        .main_metric('ndcg@10')
-        .source_models(*SOURCE_MODELS)
-        .sid_variants(*SID_VARIANTS)
-        .uid_variants(*UID_VARIANTS)
-        .grid(
-            'basic_llm',
-            datasets=DATASETS,
-            models=LLM_MODELS,
-            targets=['uid', 'sid'],
-            histories=LLM_HISTORIES,
-        )
-        .grid(
-            'basic_scratch',
-            datasets=DATASETS,
-            models=SCRATCH_MODELS,
-            targets=['uid', 'sid'],
-            histories=SCRATCH_HISTORIES,
-        )
-    ).export(Path('config/basic_scheduler.yaml'))
-
-
 def build_simple_schedule():
     return (
         Schedule(
@@ -50,7 +11,7 @@ def build_simple_schedule():
         )
         .main_metric('ndcg@10')
         .grid(
-            'simple_uid2uid',
+            'simple',
             datasets=['mind'],
             models=['scratch'],
             targets=['uid'],
@@ -60,4 +21,3 @@ def build_simple_schedule():
 
 
 build_simple_schedule()
-
