ffe9ad94a6bfea4e5f71f0dc9a609d1d1b0be35b
Jyonn <liu@qijiong.work>
2026-07-06 21:23:10 +0800

    Move trained search utility to root

--- searcher.py
+++ searcher.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 from pathlib import Path
 
 
-ROOT = Path(__file__).resolve().parents[1]
+ROOT = Path(__file__).resolve().parent
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
